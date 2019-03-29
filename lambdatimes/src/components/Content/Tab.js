e1a15bca41bb0c5e9f1122e25d6cb88a6ff535fa
jurusteve <jstevemu17@gmail.com>
2019-03-29 12:57:38 -0400

    completed mvp and part of stretch

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,22 +1,50 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
   return (
-    <div
-      className={''}
+    <TabStyles
+      active={props.tab === props.selectedTab ? "active-tab" : ""}
       onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
+        props.selectTabHandler(props.tab);
       }}
     >
       {props.tab.toUpperCase()}
-    </div>
+    </TabStyles>
   );
 };
+Tab.defaultProp = {
+  active: PropTypes.string
+};
+const activeTabMixin = css`
+  background-color: #fff;
+  color: #333;
+  border: 2px solid #333;
+`;
 
-// Make sure you include PropTypes on your props.
+const TabStyles = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    text-decoration: underline;
+  }
+  ${props => (props.active ? activeTabMixin : "")}
+`;
 
+Tab.propTypes = {
+  tab: PropTypes.string.isRequired,
+  selectedTab: PropTypes.string.isRequired,
+  selectTabHandler: PropTypes.func.isRequired
+};
 export default Tab;
