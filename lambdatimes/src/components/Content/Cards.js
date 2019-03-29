1418f80bb6190ec83c6d8a4d0f151b4dba701255
jurusteve <jstevemu17@gmail.com>
2019-03-29 14:08:19 -0400

    completed part 1 of stretch

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Card from "./Card";
+import styled from "styled-components";
 
 const Cards = props => {
   return (
-    <div className="cards-container">
+    <CardsStyles>
       {props.cards.map((card, i) => {
         return <Card card={card} key={i} />;
       })}
-    </div>
+    </CardsStyles>
   );
 };
+
+const CardsStyles = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: none;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 16px;
+  flex-wrap: wrap;
+  @media (min-width: 1200px) {
+    width: 1200px;
+    margin: 0 auto;
+  }
+`;
 
 Card.propTypes = {
   cards: PropTypes.array
